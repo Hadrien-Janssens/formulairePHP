9efdd5a5dc4f0c325ae17edfee8c0070fd234689
BlackPigCompany <86426768+BlackPigCompany@users.noreply.github.com>
2024-03-10 12:10:58 +0100

    sauvegarde avant modif sur la boucle principale

--- contact.php
+++ contact.php
@@ -6,22 +6,50 @@ require_once "./header.php";
 $erreurs = traitementFormulaireContact($_POST);
 ?>
 
-<div class="container">
-    <h1>Contactez-moi</h1>
-
-
-    <form action="" method="POST">
-        <input type="hidden" name="formName" value="formContact">
-        <label for="name">Nom</label>
-        <input type="text" maxlength="255" name="name" id="name">
-        <?=   $erreurs['name'] ?? 'euh..' ?>
-        <label for="firstname">Prénom</label>
-        <input type="text" name='firstname' maxlength="255" minlength="2" id="firstname">
-        <?=   $erreurs['firstname'] ?? 'euh..hein' ?>
-        <label for="mail">Mail</label>
-        <input type="email" name='email' required id="mail">
-        <label for="message">Message</label>
-        <input type='text' required maxlength="3000" minlength="10" name="message">
-        <input type="submit" value="Envoyer">
-    </form>
+<div class="form-container">
+    <!-- le message d'envoie avec succes -->
+    <?php 
+        if ( (isset($_POST['name'])) && (isset($_POST['email'])) && (isset($_POST['message'])) && isset($erreurs) && empty($erreurs)) {
+           echo "<div class='succes'> Données envoyées avec succès </div>";
+        }
+    ?>
+    <div class="layout">
+        <div class="left">
+            <div>
+                <h2>
+                    Vous avez une question ? <br />
+                    <br />
+                    N'hésitez pas !
+                </h2>
+            </div>
+        </div>
+        <form action="" method="POST">
+            <input type="hidden" name="formName" value="formContact">
+            <label for="name">Nom</label>
+            <input type="text" maxlength="255" name="name" id="name"
+                value=<?php if (isset($_POST['name']) && !empty($erreurs)) echo $_POST['name'] ; ?>>
+            <div class="erreur">
+                <?=   $erreurs['name'] ?? '' ?>
+            </div>
+            <label for="firstname">Prénom</label>
+            <input type="text" name='firstname' maxlength="255" minlength="2" id="firstname"
+                value=<?php if (isset($_POST['firstname']) && !empty($erreurs)) echo $_POST['firstname'] ; ?>>
+            <div class="erreur">
+                <?=   $erreurs['firstname'] ?? '' ?>
+            </div>
+            <label for="email">Mail</label>
+            <input type="" name='email' id="email"
+                value=<?php if (isset($_POST['email']) && !empty($erreurs)) echo $_POST['email'] ; ?>>
+            <div class="erreur">
+                <?=   $erreurs['email'] ?? '' ?>
+            </div>
+            <label for="message">Message</label>
+            <input type='text' required maxlength="3000" minlength="10" name="message"
+                value=<?php if (isset($_POST['message']) && !empty($erreurs)) echo $_POST['message'] ; ?>>
+            <div class="erreur">
+                <?=   $erreurs['message'] ?? '' ?>
+            </div>
+            <input type="submit" value="Envoyer">
+        </form>
+    </div>
 </div>
--- traitementFormulaireContact.php
+++ traitementFormulaireContact.php
@@ -1,43 +1,55 @@
 <?php
 function traitementFormulaireContact($dataArray) {
     $erreurs = [];
+    $regles = [
+        "name" => [
+          "require" => true,
+          "max" => 255,
+          "min" => 2
+        ],
+        "firstname" => [
+          "require" => false,
+          "max" => 255,
+          "min" => 2
+        ],
+        "email" => [
+            "require" => true,
+            "max" => 255,
+            "min" => 2
+        ],
+        "message" => [
+            "require" => true,
+            "max" => 3000,
+            "min" => 2
+          ]
+        ];
     $dataArray = xssSecurity($dataArray);
-    if (!checkIfGoodData($dataArray)) {
-        $erreurs['name']="champs requis !";
-        $erreurs['name']="champs requis !";
-    }
-    else {
-        if (!checkNumberCharactere($dataArray["name"],2,255)) {
-            $erreurs["name"]="nombre de caractere incorrect !";
+    foreach ($dataArray as $key => $value) {
+        
+        if (isset($regles[$key])  ) {
+
+            if( $regles[$key]['require'] && empty($dataArray[$key]) ) {
+                $erreurs[$key]="champs requis !";
+            }
+            elseif ( $regles[$key]['max'] < strlen($dataArray[$key]) || $regles[$key]['min'] > strlen($dataArray[$key])) {
+                if (!empty($dataArray[$key])) {
+                    $erreurs[$key]="Ce champs doit contenir entre ". $regles[$key]['min'] ." et ". $regles[$key]['max'] ." caractères";
+                }
+            }
+            elseif (!filter_var($dataArray['email'], FILTER_VALIDATE_EMAIL)) {
+                if (!isset($erreurs['email'])) {
+                    $erreurs['email']="Veuillez entrer une adresse mail valide";
+                }
+            }
         }
     }
     return $erreurs;
 }
+
 function xssSecurity($array) {
     foreach ($array as $key => $value) {
         $newValue = htmlentities($array[$key]);
         $array[$key]=$newValue;
     }
     return $array;
-}
-function checkIfGoodData(array $dataArray):bool {
-    $require = [
-        "name",
-        "email",
-        "message"
-    ]  ;
-    foreach ($dataArray as $key => $value) {
-        if (in_array($key,$require) ) {
-           if ((!isset($dataArray[$key]) || empty($dataArray[$key]))) {
-               return false;
-          }
-        }
-    }
- return true;
-}
-function checkNumberCharactere (string $string, int $min , int $max):bool{
-    if (strlen($string) >= $min && strlen($string) <= $max) {
-        return true;
-    }
-    return false; 
 }
